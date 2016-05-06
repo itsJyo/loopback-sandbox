bbde5008df6d88210274f3aa802981f5e5712aeb
jyothsna <jyothsna_shriyan@edgeverve.com>
2016-05-06 14:07:39 +0530

    added a employee model

---
+++ common/models/employee.js
@@ -0,0 +1,29 @@
+module.exports = function(Employee) {
+var loopback = require('loopback');
+
+ Employee.createEmployee = function(data, cb) {
+ //get the loopback current context
+		var ctx = loopback.getCurrentContext();
+		//setting some dummy value
+		ctx.set('tenantid', 'ev');
+		//logging the current context before rest call
+		console.log('current Context Before Create :', ctx);
+		Employee.create(data,function(err,repsonse){
+		console.log('current Context after create :', ctx);
+		if(err){
+		cb(err);
+		}
+		cb(null, 'success... ' ,data);
+		});
+      
+    }
+
+Employee.remoteMethod(
+    'createEmployee', 
+    {
+       accepts: {arg: 'data', type: 'object'},
+       returns: {arg: 'status', type: 'boolean'},
+       http: {path: '/createEmployee', verb: 'get'}
+     }
+);
+};
